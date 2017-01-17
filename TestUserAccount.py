8b96b75a853f48f835996253a0cbe3b1090cda07
einav18-meet <einavcohen48@gmail.com>
2017-01-17 18:54:30 +0200

    einav class

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -6,7 +6,7 @@ my_user=UserAccount("einav18-meet","meetyear18","I LOVE PIZZA")
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
 #Use the wrong password as input here
-my_user.print_secret("fgvzdf")
+my_user.print_secret("meetyear18")
 
 #Use the right password here
-my_user.print_secret("meetyear18")
+
---
+++ student.py
@@ -0,0 +1,17 @@
+class Student :
+    
+    def __init__ (self,name,homeTown,age,height,favoriteIceCream):
+
+        self.name= name
+        self.homeTown = homeTown
+        self.age = age
+        self.height = height
+        self.favoriteIceCream = favoriteIceCream
+
+    def print_summary(self):
+
+
+        print("the name of the student is "+self.name+" he is age is "+self.age+" and home town is "+self.homeTown+" he is heigh "+self.height+"and his favorute ice cream is "+self.favoriteIceCream)
+
+
+        
