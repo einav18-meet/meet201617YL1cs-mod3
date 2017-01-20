b27857119a44649f04b4aa0d34bf8f679e46ecb0
einav18-meet <einavcohen48@gmail.com>
2017-01-20 09:38:05 +0200

    Update student.py

--- student.py
+++ student.py
@@ -9,9 +9,11 @@ class Student :
         self.favoriteIceCream = favoriteIceCream
 
     def print_summary(self):
-
-
         print("the name of the student is "+self.name+" he is age is "+self.age+" and home town is "+self.homeTown+" he is heigh "+self.height+"and his favorute ice cream is "+self.favoriteIceCream)
 
-
+    def get_giraffe_gap(self, heigh):
+        self.heigh = heigh
+        average = 500-self.heigh
         
+        
+
